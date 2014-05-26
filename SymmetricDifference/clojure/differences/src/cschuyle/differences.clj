c64cfc59dc53e56dee16f98b1e70b8981aac3147
Carlton Schuyler <carl@dragnon.com>
2014-05-26 09:48:18 -0600

    Made symmetric-difference Clojure multi-arity

--- SymmetricDifference/clojure/differences/src/cschuyle/differences.clj
+++ SymmetricDifference/clojure/differences/src/cschuyle/differences.clj
@@ -9,8 +9,14 @@
       (update-in m [k] dec)))
 
 (defn symmetric-difference
-  [a b]
-  (as-> {} acc
-        (reduce (fn [m k] (vinc m k)) acc a)
-        (reduce (fn [m k] (vdec m k)) acc b)
-        (reduce-kv (fn [l k v] (if (= v 0) l (concat l (take (Math/abs v) (repeat k))))) [] acc)))
+  ([a] a)
+  ([a b]
+     (as-> {} acc
+           (reduce (fn [m k] (vinc m k)) acc a)
+           (reduce (fn [m k] (vdec m k)) acc b)
+           (reduce-kv (fn [l k v] (if (= v 0) l (concat l (take (Math/abs v) (repeat k))))) [] acc)))
+  ([a b & more]
+     (apply symmetric-difference
+            (symmetric-difference (symmetric-difference a b) (first more))
+            (rest more))))
+
--- SymmetricDifference/clojure/differences/test/cschuyle/differences_test.clj
+++ SymmetricDifference/clojure/differences/test/cschuyle/differences_test.clj
@@ -7,3 +7,7 @@
   (is (empty? (symmetric-difference nil nil)))
   (is (empty? (symmetric-difference [:b :a] [:a :b])))
   (is (= [1 4 4 5]) (sort (symmetric-difference #{1 2 3} [2 3 4 4 5]))))
+
+(deftest larger-arity
+  (is (= [:a] (symmetric-difference [:a :a :b :c] [:b :b :c] [:a :b])))
+  (is (empty? (symmetric-difference [:a :a :b :c] [:b :b :c] [:a :b] [:a]))))
