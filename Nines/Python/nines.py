6fbdb7c99dec05b26ecb5017364374d08b8acca4
Carlton Schuyler <carl@dragnon.com>
2016-02-18 01:41:18 -0700

    More feeble attempts at optimization, I should try to get Contextual to work

--- Nines/Python/nines.py
+++ Nines/Python/nines.py
@@ -3,12 +3,33 @@ pair_solutions = {}
 def pair_solution(a,b):
     if((a,b) in pair_solutions): return pair_solutions[(a,b)]
     solutions = []
-    solutions.append(a * b)
-    solutions.append(a + b)
+    if(a == 0 or b == 0):
+        solutions.append(0)
+    elif(a == 1):
+        solutions.append(b)
+    elif(b == 1):
+        solutions.append(a)
+    else:
+        solutions.append(a * b)
+
+    if(a == 0):
+        solutions.append(b)
+    elif(b == 0):
+        solutions.append(a)
+    else:
+        solutions.append(a + b)
+
     solutions.append(a - b)
-    if(a != b): solutions.append(b - a)
-    if(a != 0): solutions.append(b / a)
-    if(b != 0 and a != b and a != -b): solutions.append(a / b)
+
+    if(a != b):
+        if(a != 0):
+            solutions.append(b - a)
+        if(b != 0 and b != 1 and a != -b):
+            solutions.append(a / b)
+
+    if(a != 0 and a != 1):
+        solutions.append(b / a)
+
     frozen = tuple(solutions)
     pair_solutions[(a,b)] = frozen
     return frozen
